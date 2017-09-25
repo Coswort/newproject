c000ace88d2c2fe7ab12929093c0215e191fad4d
Coswort <a-bakur@mail.ru>
2017-09-25 16:29:33 +0300

    update my file

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+#1
 import random
 
 numbers = []
@@ -21,3 +22,59 @@ else:
     median = sum(numbers[half_size - 1:half_size + 1]) / 2
 
 print(median)
+
+
+#2
+with open('second.py', 'r+') as f:
+    #f.write('Здарова братан!')
+    print(f.read)
+
+
+#3
+def some_func(abc, param):
+    return abc(*param)
+
+def abc(name):
+    print('Hello {}'.format(name))
+
+some_func(abc, ['friend'])
+
+
+#4
+print(list(map(lambda x: x ** 2, range (1, 10))))
+print(list(filter(lambda x: x > 5, range (1, 10))))
+
+
+#5
+def str_list(num_list):
+    return list(map(str, num_list))
+
+print(str_list(range(5)))
+
+
+#6
+from functools import reduce
+
+def some_func(par):
+    return par
+
+print(reduce(some_func, ['тест']))
+
+
+#7
+from functools import partial
+
+def func(firstname, secondname):
+    return '{} {}'.format(firstname, secondname)
+
+hier = partial(func, secondname = 'Bakursky')
+
+print(hier('Andrey'))
+
+
+#8
+num_list = range(6)
+squared_list = [x ** 2 for x in num_list]
+
+print(list(zip(num_list, squared_list)))
+
